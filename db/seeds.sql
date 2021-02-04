799a0a60cfe7208b2c80ad2e70a963ea206e990f
James Howard <jthoward09@gmail.com>
2021-02-03 21:47:51 -0500

    Create database schema
    Creates the seed files and schema

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Whooper', false);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Mac', true);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Quarter Pounder', true);
